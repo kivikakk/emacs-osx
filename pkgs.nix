35b63a8602f796540edbc69d014b3af8e23a7ff5
Tim Steinbach <tim.steinbach@shopify.com>
2022-09-11 18:14:53 -0400

    Update nixpkgs

--- pkgs.nix
+++ pkgs.nix
@@ -1,11 +1,11 @@
 let
   # refer https://status.nixos.org for latest nixpkgs
-  commit = "30d3d79b7d3607d56546dd2a6b49e156ba0ec634";
+  commit = "17352e8995e1409636b0817a7f38d6314ccd73c4";
   pkgs = import (fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/${commit}.tar.gz";
 
     # nix-prefetch-url --unpack \
     # https://github.com/NixOS/nixpkgs/archive/b2d256095aeac09f8af6b6c95ab3cf4bc5fd4e6f.tar.gz
-    sha256 = "sha256:0x5j9q1vi00c6kavnjlrwl3yy1xs60c34pkygm49dld2sgws7n0a";
+    sha256 = "1cb9p9hfbwwnjaxqxh1fammq9hcrm2qghqyjzvprrk1vcq7dzhxc";
   }) { overlays = [ (import ./emacs-overlay.nix) ]; };
 in pkgs
